83be69dd2f048d8517193beb91b9834b3af82a9c
Rafael Dias <rafaelsd70@gmail.com>
2024-07-29 20:52:33 -0300

    feat: criado placeholder para parâmetro uuid | criado a rota de autenticação

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,19 +6,26 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->set404Override('App\Controllers\Api\Errors::NotFound');
+$routes->addPlaceholder('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
+
+/**
+ * Authentication api route
+ */
+$routes->group('auth', ['namespace' => 'App\Controllers\Api'],  static function ($routes) {
+    $routes->post('/', 'Auth::login');
+});
 
 /**
  * @var RouteCollection $routes
  */
-$routes->group('api', ['namespace' => 'App\Controllers\Api'], static function ($routes) {
+$routes->group('api', ['namespace' => 'App\Controllers\Api', 'filter' => 'AuthFilter'], static function ($routes) {
 
     $routes->group('task', static function ($routes) {
-
         $routes->get('/', 'Task::index');
     });
 
-    $routes->group('auth', static function ($routes) {
-
-        $routes->post('/', 'Auth::login');
+    $routes->group('projects', static function ($routes) {
+        $routes->get('/', 'Project::index');
+        $routes->get('/(:uuid)', 'Project::GetProjectByUID/$1');
     });
 });
