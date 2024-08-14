1aeae3974a5fde5f6ecea685f3c7437e2608d15d
Rafael Dias <rafaelsd70@gmail.com>
2024-08-14 20:23:23 -0300

    feat: incluido alguns dados do user no token

--- app/Helpers/jwt_helper.php
+++ app/Helpers/jwt_helper.php
@@ -10,14 +10,10 @@ function generateJWT($user)
         'iss' => base_url(),
         'aud' => base_url(), // Audiência do token, ou seja, quem é o público-alvo do token
         'sub' => $user->id_user,
-        'iat' => time(), // Data e hora de emissão do token
-        'exp' => time() + 60, // Data de expiração do token
+        'iat' => strtotime('now'), // Data e hora de emissão do token
+        'exp' => strtotime('+10 minutes'), // Data de expiração do token
         'data' => [
-            'user' => [
-                'email' => $user->email_user,
-                'firstname' => $user->firstname_user,
-                'lastname' => $user->lastname_user,
-            ]
+            'user' => $user
         ]
     ];
 
