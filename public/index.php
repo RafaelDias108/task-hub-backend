97345c82ed843b3efc7f860aec849b0cf7fbdf48
RafaelDias108 <rafaelsd70@gmail.com>
2025-02-17 22:56:44 -0300

    feat: configurado o CORS da API

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php
 
+
+header('Access-Control-Allow-Origin: *');
+
+header("Access-Control-Allow-Methods:  GET, POST, PUT, PATCH, DELETE, OPTIONS");
+
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+	header("HTTP/1.1 200 OK");
+	die();
+}
+
 /*
  *---------------------------------------------------------------
  * CHECK PHP VERSION
